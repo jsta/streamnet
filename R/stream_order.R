e797cedd4662880b89bc9fb6a9dbe81b97d245ee
jsta <stachel2@msu.edu>
2017-10-03 15:15:22 -0400

    namespace fixes

--- R/stream_order.R
+++ R/stream_order.R
@@ -79,40 +79,42 @@ stream_order <- function(lines, outlet, ...){
 #' function should be used in most cases.
 #' @param tree igraph tree object
 #'
-#' @importFrom igraph degree as_edgelist
+#' @importFrom igraph degree as_edgelist delete.vertices vcount E V
 #' @export
 #'
 #' @examples \dontrun{
 #' tree <- create_reversed_tree(15)
-#' E(tree)$weight <- stream_order_igraph(tree)
-#' plot(tree, edge.label = E(tree)$weight)
+#' igraph::E(tree)$weight <- stream_order_igraph(tree)
+#' plot(tree, edge.label = igraph::E(tree)$weight)
 #' }
-
 stream_order_igraph <- function(tree){
   base_order <- 1
-  leaf_nodes <- names(which(degree(tree, v = V(tree), mode = "in") == 0,
+  leaf_nodes <- names(which(degree(tree,
+                                   v = igraph::V(tree),
+                                   mode = "in") == 0,
                             useNames = TRUE))
-  edgelist <- data.frame(as_edgelist(tree))
+  edgelist   <- data.frame(as_edgelist(tree))
   edgelist$order <- NA
   names(edgelist)[c(1,2)] <- c("from", "to")
   edgelist$order[edgelist$from %in% leaf_nodes] <- base_order
 
-  tree <- delete.vertices(tree, leaf_nodes)
+  tree <- igraph::delete.vertices(tree, leaf_nodes)
 
-  while(vcount(tree) >= 1){
+  while(igraph::vcount(tree) >= 1){
     base_order <- max(edgelist$order, na.rm = TRUE) + 1
-    leaf_nodes <- names(which(degree(tree, v = V(tree), mode = "in") == 0,
+    leaf_nodes <- names(which(degree(tree, v = igraph::V(tree),
+                                     mode = "in") == 0,
                               useNames = TRUE))
 
     raised_nodes <- sapply(leaf_nodes,
-                           function(x) all(edgelist$order[edgelist$to == x] == base_order - 1))
+          function(x) all(edgelist$order[edgelist$to == x] == base_order - 1))
     raised_nodes <- names(which(raised_nodes))
     flat_nodes <- leaf_nodes[!(leaf_nodes %in% raised_nodes)]
 
     edgelist$order[edgelist$from %in% raised_nodes] <- base_order
     edgelist$order[edgelist$from %in% flat_nodes] <- base_order - 1
 
-    tree <- delete.vertices(tree, leaf_nodes)
+    tree <- igraph::delete.vertices(tree, leaf_nodes)
 
   }
   edgelist$order
