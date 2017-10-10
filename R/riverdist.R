0ad3706d48a198db6006b09db91079b5bbb347df
jsta <stachel2@msu.edu>
2017-10-10 16:04:02 -0400

    add crs argument

--- R/riverdist.R
+++ R/riverdist.R
@@ -1,11 +1,12 @@
 #' Rivernetwork to sf object
 #'
 #' @param rvnet rivernetwork object from the riverdist package
+#' @param crs crs string or epsg code
 #'
 #' @importFrom sf st_multilinestring st_sfc st_cast st_sf
 #'
 #' @export
-rvnet2sf <- function(rvnet){
+rvnet2sf <- function(rvnet, crs){
   res_geom <- st_cast(st_sfc(st_multilinestring(rvnet$lines)), "LINESTRING")
-  st_sf(rvnet$lineID, geom = res_geom)
+  st_sf(rvnet$lineID, geom = res_geom, crs = crs)
 }
