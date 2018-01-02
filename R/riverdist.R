5f0cb1574729e7c95ecdfd9f4020b3174b7a09a0
jsta <stachel2@msu.edu>
2018-01-02 14:51:20 -0500

    add option to not dissolve, retain data table if not

--- R/riverdist.R
+++ R/riverdist.R
@@ -22,15 +22,40 @@ rvnet2sf <- function(rvnet, crs){
 #' @importFrom riverdist removeduplicates dissolve removemicrosegs setmouth trimriver detectroute riverdirection
 #' @export
 #' @examples \dontrun{
+#' library(nhdR)
+#' library(sf)
+#'
+#' # Generate test dataset
+#' bbox <- data.frame(xmin = -73.33838, ymin = 41.35841,
+#' xmax = -73.14540, ymax = 41.48593)
+#' b0 <- sf::st_sfc(sf::st_polygon(list(rbind(
+#'   c(bbox$xmin, bbox$ymin),
+#'   c(bbox$xmax, bbox$ymin),
+#'   c(bbox$xmax, bbox$ymax),
+#'   c(bbox$xmin, bbox$ymax),
+#'   c(bbox$xmin, bbox$ymin)))))
+#' sf::st_crs(b0) <- 4326
+#' b0 <- st_transform(b0, sf::st_crs(nhdR::vpu_shp))
+#'
+#' outlet_reach <- terminal_reaches(
+#'   lon = st_coordinates(st_centroid(b0))[1],
+#'   lat = st_coordinates(st_centroid(b0))[2],
+#'   approve_all_dl = TRUE)
+#' outlet_point <- st_cast(st_line_sample(outlet_reach, sample = 1), "POINT")
+#'
+#' nhd <- nhd_plus_query(poly = b0, dsn = c("NHDFlowLine"))$sp$NHDFlowLine
 #' mouthseg  <- which(nhd$COMID == outlet_reach$comid)
 #  mouthvert <- 1
 #  nhd_rv <- riverdist::line2network(as(nhd, "Spatial"), tolerance = 1)
 #' res <- autoclean(nhd_rv, mouthseg, mouthvert, crs = st_crs(nhd))
 #' }
-autoclean <- function(rivernetwork, mouthseg, mouthvert, crs){
-
+autoclean <- function(rivernetwork, mouthseg, mouthvert, crs, dissolve = FALSE){
   rivernetwork <- riverdist::removeduplicates(rivers = rivernetwork)
-  rivernetwork <- riverdist::dissolve(rivernetwork)
+
+  if(dissolve){
+    rivernetwork <- riverdist::dissolve(rivernetwork)
+  }
+
   rivernetwork <- removemicrosegs(rivernetwork)
 
   rivernetwork <- setmouth(seg = mouthseg,
@@ -80,7 +105,9 @@ autoclean <- function(rivernetwork, mouthseg, mouthvert, crs){
   # below_mouth  <- which(below_mouth)
   # rivernetwork <- trimriver(rivers = rivernetwork, trim = below_mouth)
 
-  rivernetwork_geom <- st_cast(st_sfc(st_multilinestring(rivernetwork$lines)), "LINESTRING")
-  st_sf(rivernetwork$lineID, geom = rivernetwork_geom, crs = crs)
+  rivernetwork_geom <- st_cast(st_sfc(st_multilinestring(rivernetwork$lines)),
+                               "LINESTRING")
+  st_sf(cbind(rivernetwork$lineID, rivernetwork$sp@data),
+        geom = rivernetwork_geom, crs = crs)
   # st_as_sf(rivernetwork)
 }
