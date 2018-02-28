ff5d0afacf9272319c6b14e019696a9cc89722c6
jsta <stachel2@msu.edu>
2018-02-28 12:37:04 -0500

    return original line if nrow == 1

--- R/avg_link_length.R
+++ R/avg_link_length.R
@@ -25,11 +25,18 @@ avg_link_length <- function(lines, simplify = FALSE){
 #' @export
 #' @importFrom sf st_union st_line_merge
 #' @examples \dontrun{
-#' data(nhd_sub)
-#' res <- simplify_network(nhd_sub)
+#' data(nhd_sub_lines)
+#' res <- simplify_network(nhd_sub_lines)
+#'
+#' # single line returns the original
+#' res <- simplify_network(nhd_sub_lines[1,])
 #'
 #' }
 simplify_network <- function(lines){
-  st_cast(st_line_merge(
-    st_union(st_cast(lines, "MULTILINESTRING"))), "LINESTRING")
+  if(nrow(lines) > 1){
+    st_cast(st_line_merge(
+      st_union(st_cast(lines, "MULTILINESTRING"))), "LINESTRING")
+  }else{
+    lines
+  }
 }
